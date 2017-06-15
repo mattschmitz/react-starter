0f7f5b19e4e73416c42bf1be5567b5cc4ff47b68
Matt Schmitz <mattschmitz9@gmail.com>
2017-06-15 10:34:41 -0700

    setup webpack and dev server

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+}
